90eea7c077876a676b17b982862a128fda44cec9
Wenqiang Xie <wq.xie@outlook.com>
2020-11-09 22:16:37 +0800

    重构微信小程序富文本阅读模块

---
+++ client/src/pages/viewer/index.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import Taro, { getCurrentInstance } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+
+export default function () {
+    return (
+        <View></View>
+    );
+}
