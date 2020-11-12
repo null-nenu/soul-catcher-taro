b76576806536af789c6f6848d432cfdbc26c263b
Wenqiang Xie <wq.xie@outlook.com>
2020-11-12 22:16:12 +0800

    重构小程序用户账户模块

---
+++ client/src/pages/account/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import Taro from "@tarojs/taro";
+import { View } from "@tarojs/components";
+
+import "./index.css";
+
+export default function Account() {
+    return (
+        <View className="index"></View>
+    );
+}
