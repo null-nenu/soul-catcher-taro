3a0139a172a56ccd0a9b74352185cb87d173b8e8
Wenqiang Xie <wq.xie@outlook.com>
2020-11-13 10:47:20 +0800

    重构关于应用模块

---
+++ client/src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Taro from "@tarojs/taro";
+import { View } from "@tarojs/components";
+
+/**
+ * 
+ */
+export default function About() {
+    return (
+        <View className="index"></View>
+    );
+}
